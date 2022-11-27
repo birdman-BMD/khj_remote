6daf260198d583cc961736405e940754970a1892
birdman-BMD <birdyaztt@gmail.com>
2022-11-27 15:13:20 +0300

    Initial Commit

---
+++ khj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  khj
+//
+//  Created by yahya almarshoud on 27/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
